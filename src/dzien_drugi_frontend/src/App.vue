da04128e0477c956ce037903495f99ecbfffdeef
szymongrodek <thentcd@gmail.com>
2024-06-26 19:29:40 +0200

    End of day three

--- src/dzien_drugi_frontend/src/App.vue
+++ src/dzien_drugi_frontend/src/App.vue
@@ -1,33 +1,12 @@
 <script setup>
-import { ref } from 'vue';
-import { dzien_drugi_backend } from 'declarations/dzien_drugi_backend/index';
 import Blog from './components/Blog.vue';
-let greeting = ref('');
-
-async function handleSubmit(e) {
-  e.preventDefault();
-  const target = e.target;
-  const name = target.querySelector('#name').value;
-  const surname = target.querySelector('#surname').value;
-  await dzien_drugi_backend.greet(name, surname).then((response) => {
-    greeting.value = response;
-  });
-}
 </script>
 
 <template>
-  <main>
+  <main class="container mx-auto">
     <img src="/logo2.svg" alt="DFINITY logo" />
     <br />
     <br />
-    <h1 class="text-blue-600">Siema?!</h1>
-    <form action="#" @submit="handleSubmit">
-      <label for="name">Enter your name and surname: &nbsp;</label>
-      <input id="name" alt="Name" type="text" />
-      <input id="surname" alt="Surname" type="text" />
-      <button type="submit">Greet Me!</button>
-    </form>
-    <section id="greeting">{{ greeting }}</section>
     <Blog />
   </main>
 </template>
--- src/dzien_drugi_frontend/src/components/Blog.vue
+++ src/dzien_drugi_frontend/src/components/Blog.vue
@@ -1,8 +1,30 @@
 <template>
-  <button @click="fetchPosts">Fetch</button>
   <div>
-    siema blog! {{ posts }} <input v-model="newPost" type="text" />
-    <button @click="addPost">Add</button>
+    <h2 class="text-blue-600">Tell me something about yourself, my Blog</h2>
+    <div class="w-100 flex flex-row-reverse">
+      <button @click="fetchPosts" class="bg-blue-600 rounded text-white p-4">
+        Fetch
+      </button>
+    </div>
+    <div class="grid mx-6 gap-4 my-4">
+      <div
+        v-for="(post, index) in posts"
+        class="drop-shadow-xl bg-stone-300 p-4"
+      >
+        <p>id: {{ index }}</p>
+        <p>{{ post }}</p>
+      </div>
+    </div>
+    <div class="flex justify-center flex-col">
+      <input
+        v-model="newPost"
+        class="border-2 border-blue-600 p-4"
+        type="text"
+      />
+      <button @click="addPost" class="bg-blue-600 rounded text-white p-4">
+        Add
+      </button>
+    </div>
   </div>
 </template>
 
@@ -22,6 +44,9 @@ export default {
     async fetchPosts() {
       this.posts = await dzien_drugi_backend.read_posts();
     },
+  },
+  async mounted() {
+    this.fetchPosts();
   },
 };
 </script>
